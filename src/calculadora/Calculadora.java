55ed7b509c07d39d0fc4665cbad0f1ea6d109cf1
paulocer <paulocer@gmail.com>
2023-01-04 20:13:17 -0300

    Variáveis da classe criadas.

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author paulo
+ */
+public class Calculadora {
+
+        /**
+         * 1. Declaração de variáveis de classe
+         */
+        static int x, y;
+        static int soma;
+        static int subtracao;
+        static int multiplicacao;
+        static int divisao;
+    
+    public static void main(String[] args) {
+        
+        /**
+         * 1. Declaração de variáveis locais (main)
+         */
+         Scanner scanner = new Scanner(System.in);
+
+         /**
+          * 2. Entrada de dados
+         */
+        System.out.print ("Digite o valor de x: ");
+        x = scanner.nextInt();
+        System.out.print ("Digite o valor de y: ");
+        y = scanner.nextInt();
+
+         /**
+          * 3. Chamada dos métodos
+         */
+        soma();
+        subtracao();
+        multiplicacao();
+        divisao();
+}
+
+    public static void soma() {
+
+        /**
+         * 1. Declaração de variáveis
+         */
+
+         /**
+          * 2. Entrada de dados
+          * 
+         */
+
+         /**
+          * 3. Processamento
+         */
+        soma = x + y;
+ 
+         /** 4. Saíde da dados
+         */
+        System.out.println("[método soma] Soma = " + soma);
+
+    }
+    
+    public static void subtracao() {
+
+        /**
+         * 1. Declaração de variáveis
+         */
+
+         /**
+          * 2. Entrada de dados
+          * 
+         */
+
+         /**
+          * 3. Processamento
+         */
+        subtracao = x - y;
+ 
+         /** 4. Saíde da dados
+         */
+        System.out.println("[método subtração] Subtração = " + subtracao);
+
+    }
+    
+    public static void multiplicacao() {
+
+        /**
+         * 1. Declaração de variáveis
+         */
+
+         /**
+          * 2. Entrada de dados
+          * 
+         */
+
+         /**
+          * 3. Processamento
+         */
+        multiplicacao = x * y;
+ 
+         /** 4. Saíde da dados
+         */
+        System.out.println("[método multiplicação] Multiplicação = " + multiplicacao);
+
+    }
+
+    public static void divisao() {
+
+        /**
+         * 1. Declaração de variáveis
+         */
+
+         /**
+          * 2. Entrada de dados
+          * 
+         */
+
+         /**
+          * 3. Processamento
+         */
+        divisao = x / y;
+ 
+         /** 4. Saíde da dados
+         */
+        System.out.println("[método divisão] Divisão = " + divisao);
+
+    }
+
+}
